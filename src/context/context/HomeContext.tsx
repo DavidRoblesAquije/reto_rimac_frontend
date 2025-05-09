9b693153a7e723f6aa4d9c8c82fb23e5016e94f5
DavidRoblesAquije <draquije2010@hotmail.com>
2025-05-08 22:13:53 -0500

    modificacion type HomeContext

--- src/context/context/HomeContext.tsx
+++ src/context/context/HomeContext.tsx
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import { type FormContextType } from "../../types/form";
+import type { FormContextType } from "../../types/form";
 
 export const HomeContext = createContext<FormContextType | null>(null);
